031adc06de67a6c7278399197b5966d3d71a381e
MAHFUZ ISLAM <77238922+Mahfuzislam1695@users.noreply.github.com>
2023-05-05 22:08:00 +0600

    generic

---
+++ advance-typescript/src/ageneric-type.ts
@@ -0,0 +1,18 @@
+type GenericArray<T> = Array<T>;
+
+const rollNumber: GenericArray<number> = [1, 2, 3];
+const rollNumberString: GenericArray<string> = ["1", "2", "3"];
+const rollNumberBoolean: GenericArray<boolean> = [true, false];
+
+// ======== 
+type GenericTuple<x, y> = [x, y];
+const myData: GenericTuple<string, string> = ["Al Mamun", "Programmer"]
+/////
+
+// type RelationWithSalaryType = {name: string, salary: number};
+interface RelationWithSalaryI {name: string, salary: number};
+
+const relationSalary: GenericTuple<RelationWithSalaryI, string> = [{
+    name: "mahfuz",
+    salary: 50000,
+}, "Programmer"];
